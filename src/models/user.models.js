b8e036055e436cd72f60c5b2b2eb877bba708601
princeakanerd <princeakanerd@gmail.com>
2026-01-08 17:51:00 +0530

    Built File Upload Functionality

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-
+import bcrypt from "bcrypt"
 const userSchema = new Schema(
     {
         fullName : {
@@ -70,10 +70,21 @@ const userSchema = new Schema(
                 }
             }
         ],
-        
+
     }, {
         timestamps : true
     }
 )
+
+userSchema.pre("save", async function(next){
+    if(this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+})
+
+userSchema.method.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password) ;
+}
 
 export const User = mongoose.model("User", userSchema) ;
